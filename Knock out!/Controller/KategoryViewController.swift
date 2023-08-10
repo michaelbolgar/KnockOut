a480af661eeacca89f7ef645805b66b83fa6da22
medetkhanzhaniya <78413886+medetkhanzhaniya@users.noreply.github.com>
2023-08-10 14:23:43 +0600

    first

--- Knock out!/Controller/KategoryViewController.swift
+++ Knock out!/Controller/KategoryViewController.swift
@@ -1,13 +1,74 @@
 //
-//  KategoryView.swift
-//  Knock out!
-//
-//  Created by Михаил Болгар on 07.08.2023.
+//  ViewController.swift
+//  Dice with code
+//  Created by Zhaniya on 20.07.2023.
 //
 
-import Foundation
 import UIKit
 
-class KategoryViewController: UIViewController {
-    print("123")
+class ViewController: UIViewController {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.delegate = self
+        view.dataSource = self
+        view.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    
+    let backgroungImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let category = [UIImage(named: "p1") , UIImage(named: "p2") , UIImage(named: "p3") , UIImage(named: "p4") , UIImage(named: "p5") , UIImage(named: "p6")]
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        
+        view.addSubview(backgroungImageView)
+        view.addSubview(collectionView)
+        
+        backgroungImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroungImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroungImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroungImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
+        
+        
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 10).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        
+    }
+    
+   
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return category.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.categoryImageView.image = category[indexPath.row]
+        return cell
+    }
+    
+    
+
 }
---
+++ Knock out!/Controller/customCell.swift
@@ -0,0 +1,32 @@
+//
+//  CustomCell.swift
+//  CategoryForBomb
+//
+//  Created by Zhaniya on 10.08.2023.
+//
+import UIKit
+
+
+class CustomCell : UICollectionViewCell {
+    
+    let categoryImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(categoryImageView)
+        categoryImageView.translatesAutoresizingMaskIntoConstraints = false
+        categoryImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        categoryImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        categoryImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        categoryImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        categoryImageView.layer.cornerRadius = 20
+        categoryImageView.layer.masksToBounds = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
