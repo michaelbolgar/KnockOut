66f1380014c4966ae27df718cbd16954aafa1921
Ressevet <Ressevet05@yandex.by>
2023-08-12 13:06:10 +0300

    test

--- Knock out!/Controller/HelpViewController.swift
+++ Knock out!/Controller/HelpViewController.swift
@@ -9,9 +9,26 @@ import Foundation
 import UIKit
 
 class HelpViewController: UIViewController {
-
+    let helpView = HelpView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        layout()
         
     }
+    func layout() {
+        view.addSubview(helpView)
+        NSLayoutConstraint.activate([
+            helpView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            helpView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            helpView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            helpView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+            
+            
+        ])
+    }
+    
 }
